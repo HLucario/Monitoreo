8af5dae0ba1ce2ca9029e0230cc2166a87e4144b
HLucario <40395219+HLucario@users.noreply.github.com>
2021-04-26 14:22:33 -0500

    Pantallas

---
+++ app/src/main/java/com/example/monitoreo/AgregarHijo.kt
@@ -0,0 +1,11 @@
+package com.example.monitoreo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AgregarHijo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_agregar_hijo)
+    }
+}
