b9925be49f33c38df8223b7cf0461683e4888ec1
HLucario <40395219+HLucario@users.noreply.github.com>
2021-05-08 01:55:41 -0500

    Actividades

--- app/src/main/java/com/example/monitoreo/Registrar.kt
+++ app/src/main/java/com/example/monitoreo/Registrar.kt
@@ -2,10 +2,24 @@ package com.example.monitoreo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class Registrar : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registrar)
+    }
+    fun Registrar()
+    {
+        val editNombre = findViewById<EditText>(R.id.editNombre)
+        val nombre = editNombre.text.toString()
+        val editApPat = findViewById<EditText>(R.id.editApPat)
+        val ap_paterno = editApPat.text.toString()
+        val editApMat = findViewById<EditText>(R.id.editApMat)
+        val ap_materno = editApMat.text.toString()
+        val editCorreo = findViewById<EditText>(R.id.editNombre)
+        val correo = editCorreo.text.toString()
+        val editContra = findViewById<EditText>(R.id.editNombre)
+        val password = editContra.text.toString()
     }
 }
