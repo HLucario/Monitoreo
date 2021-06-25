696c2bc5b201cc6ae769677d7230f3df19ac3e69
HLucario <40395219+HLucario@users.noreply.github.com>
2021-06-25 10:20:26 -0500

    Fecha

--- app/src/main/java/com/example/monitoreo/AlertaResponse.kt
+++ app/src/main/java/com/example/monitoreo/AlertaResponse.kt
@@ -7,7 +7,7 @@ data class AlertaResponse(
     @SerializedName("tutor_email") val tutor_email: String,
     @SerializedName("id_hijo") val id_hijo: Int,
     @SerializedName("id_alerta") val id_alerta: Int,
-    @SerializedName("fecha") val fecha: String,
+    @SerializedName("fecha") val fecha: Date,
     @SerializedName("texto") val texto: String,
     @SerializedName("img") val img: String
 )
